9ae10fe794ff7ad55b3d2ef2980696647435d860
developomp <developomp@gmail.com>
2022-01-11 14:38:00 +0900

    converted text to icons

--- src/pages/Home/BottomButtons.svelte
+++ src/pages/Home/BottomButtons.svelte
@@ -1,16 +1,29 @@
+<script lang="ts">
+	import FaGithub from "svelte-icons/fa/FaGithub.svelte"
+	import FaDiscord from "svelte-icons/fa/FaDiscord.svelte"
+	import FaBook from "svelte-icons/fa/FaBook.svelte"
+</script>
+
 <div class="small-button-container">
-	<button>
-		<div>Docs</div>
-	</button>
-	<button>
-		<div>GitHub</div>
-	</button>
-	<button>
-		<div>Discord</div>
-	</button>
+	<button><FaBook /></button>
+	<button><FaGithub /></button>
+	<button><FaDiscord /></button>
 </div>
 
 <style lang="scss">
 	.small-button-container {
+		@apply grid gap-3 grid-cols-3 mt-5;
+
+		button {
+			@apply w-6 h-6;
+
+			&:hover {
+				@apply text-gray-200;
+			}
+
+			&:active {
+				@apply text-gray-300;
+			}
+		}
 	}
 </style>
