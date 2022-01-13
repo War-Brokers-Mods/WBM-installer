34dcb6476241b6361f3d4b1751f71c2bf5f1c045
developomp <developomp@gmail.com>
2022-01-13 11:09:50 +0900

    added data sending from backend -> frontend

--- src-tauri/src/commands/status.rs
+++ src-tauri/src/commands/status.rs
@@ -1,25 +1,45 @@
+// [SyncConst]: must be synced with `src/constants.ts`
+// [SyncStatus]: must be synced with `src/pages/Status/index.svelte`
 use crate::util;
 
+/// request types.
+/// [SyncConst]
+static LATEST_VERSION: &'static str = "LATEST_VERSION";
+static GAME_PATH: &'static str = "GAME_PATH";
+
 #[tauri::command]
-pub async fn status() {
-    println!("returning status!");
-    ping().await;
+pub async fn status(req_type: String) -> StatusData {
+    let mut res = StatusData::default();
+
+    if req_type == LATEST_VERSION {
+        res.latest_release_version = get_latest_release().await;
+    } else if req_type == GAME_PATH {
+    }
+
+    return res.clone();
 }
 
-/// ping github and get latest version at the same time
-async fn ping() -> bool {
+/// get the latest WBM release version.
+/// data is not converted to a json object because it'll be done in the front-end
+async fn get_latest_release() -> String {
     let client = util::build_client();
 
-    // get the latest version of WBM release
-    // todo: parse json
+    // todo: handle error
     let res = client
         .get("https://api.github.com/repos/War-Brokers-Mods/WBM/releases")
         .send()
         .await
+        .unwrap()
+        .text()
+        .await
         .unwrap();
 
-    println!("{:#?}", res);
+    return res;
+}
 
-    // return true if status code is 200
-    return res.status() == 200;
+/// [SyncStatus]
+#[derive(serde::Serialize, Default, Clone)]
+pub struct StatusData {
+    latest_release_version: String,
+    game_path: String,
 }
--- src/pages/Status/index.svelte
+++ src/pages/Status/index.svelte
@@ -1,12 +1,51 @@
 <script lang="ts">
 	import { invoke } from "@tauri-apps/api/tauri"
-	import { ROUTES, COMMANDS } from "../../constants"
+	import { open } from "@tauri-apps/api/shell"
+	import { ROUTES, COMMANDS, STATUS_REQUEST } from "../../constants"
 
-	function status() {
-		invoke(COMMANDS.STATUS)
+	interface StatusData {
+		latestReleaseVersion?: LatestReleaseVersion
+		gamePath?: string
+	}
+
+	interface LatestReleaseVersion {
+		name: string
+		url: string
+	}
+
+	let statusData: StatusData = {}
+
+	async function status() {
+		invoke(COMMANDS.STATUS, {
+			reqType: STATUS_REQUEST.LATEST_VERSION,
+		})
+			.then((res: any) => {
+				const data = (JSON.parse(res.latest_release_version) as any[])[0]
+				statusData.latestReleaseVersion = {
+					name: data.name,
+					url: data.html_url,
+				}
+			})
+			.catch((err) => {
+				console.error(err)
+			})
 	}
 </script>
 
 <a href={ROUTES.HOME}>Home</a>
 <br />
 <button on:click={status}>Check status!</button>
+<br />
+
+Latest version:
+{#if statusData.latestReleaseVersion}
+	<div
+		on:click={() => {
+			console.log(statusData.latestReleaseVersion.url)
+			open(statusData.latestReleaseVersion.url)
+		}}
+		style="display: inline-block; cursor: pointer"
+	>
+		{statusData.latestReleaseVersion.name}
+	</div>
+{/if}
